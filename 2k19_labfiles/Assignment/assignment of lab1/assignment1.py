094f28cac72fd97a4c29714a419ef50b49796e28
Sonjoy209164 <sonjoy209164@gmail.com>
2024-02-22 02:07:50 +0600

    laplacian

--- 2k19_labfiles/Assignment/assignment of lab1/assignment1.py
+++ 2k19_labfiles/Assignment/assignment of lab1/assignment1.py
@@ -9,6 +9,7 @@ import numpy as np
 import cv2
 import math
 
+
 def main():
     print("Assignment 1")
     color_mode = input("Enter the colormode \n RGB(1) \n GRAY(0) :")
@@ -20,7 +21,23 @@ def main():
     apply_filter_str = input("Enter the filter code \n Smoothing Filter: \n 1. Gaussian Filter\n 2. Mean Filter \n Sharpening Filter :\n 3. Laplacian Filter \n 4. LoG  \n 5. Sobel filter  : ")
     apply_filter = int(apply_filter_str)
     if apply_filter == 1:
-        kernel = get_gaussian_kernel()
+        
+        x=input("height:")
+        x=int(x)
+        y=input("width:")
+        y=int(y)
+        kernel = get_gaussian_kernel(x,y)
+        center_x=int(input("kernel center_x:"))
+        center_y=int(input("kernel_center_y:"))
+        b1, g1, r1 = cv2.split(img)
+        b1 = convolution("blue",b1, kernel,center_x,center_y)
+        g1 = convolution("green",g1 , kernel,center_x,center_y)
+        r1 = convolution("red",r1 , kernel,center_x,center_y)
+        merged = cv2.merge((b1, g1, r1))
+        print(b1)
+        cv2.imshow("merged", merged)
+        
+
     elif apply_filter == 2:
         x=input("height:")
         x=int(x)
@@ -38,13 +55,24 @@ def main():
         cv2.imshow("merged", merged)
         
         
-        
     elif apply_filter == 3:
         kernel = get_laplacian_kernel()
     elif apply_filter == 4:
         kernel = get_log_kernel()
     elif apply_filter == 5:
-        kernel = get_sobel_kernel()
+        h_v= int(input("1.Horizontal\n 2.Vertical"))
+        kernel = get_sobel_kernel(h_v)
+        center_x=int(input("kernel center_x:"))
+        center_y=int(input("kernel_center_y:"))
+        b1, g1, r1 = cv2.split(img)
+        b1 = convolution("blue",b1, kernel,center_x,center_y)
+        g1 = convolution("green",g1 , kernel,center_x,center_y)
+        r1 = convolution("red",r1 , kernel,center_x,center_y)
+        merged = cv2.merge((b1, g1, r1))
+        print(b1)
+        cv2.imshow("merged", merged)
+        
+        
     else:
         print("Invalid filter code.")
     print(kernel)
@@ -63,7 +91,6 @@ def convolution(s,img,kernel,p,q):
     padding_right = kernel.shape[1] - 1 - q
     img_bordered = cv2.copyMakeBorder(src=img, top=p, bottom=padding_bottom, left=q, right=padding_right,borderType=cv2.BORDER_CONSTANT)
     out = img_bordered.copy()
-    # cv2.imshow('bordered image', img_bordered)
 
     for i in range(p, img_bordered.shape[0] - padding_bottom - k):
         for j in range(q, img_bordered.shape[1] - padding_right - l):
@@ -76,7 +103,7 @@ def convolution(s,img,kernel,p,q):
     cv2.normalize(out, out, 0, 255, cv2.NORM_MINMAX)
     out = np.round(out).astype(np.uint8)
     print(f"normalized {out}") 
-    out = out[p: -padding_bottom, q:-padding_right]
+    #out = out[p: -padding_bottom, q:-padding_right]
     cv2.imshow(s, out)       
     return out
     
@@ -86,8 +113,21 @@ def convolution(s,img,kernel,p,q):
     
 #definition of smoothing filters 
 
-def get_gaussian_kernel():
-    
+def get_gaussian_kernel(height, width):
+    gaussian_kernel = np.zeros((height, width))
+    sigma_x = float(input("Value of sigma_X: "))  # Shape and smoothness control
+    sigma_y = float(input("Value of sigma_Y: "))
+    constant = 1 / (2 * sigma_x * sigma_y)
+    height = height // 2
+    width = width // 2
+
+    for x in range(-height, height + 1):
+        for y in range(-width, width + 1):
+            exponent = -0.5 * ((x * x / (sigma_x * sigma_x)) + (y * y / (sigma_y * sigma_y)))
+            gaussian_value = constant * math.exp(exponent)
+
+            gaussian_kernel[x + height, y + width] = gaussian_value
+
     return gaussian_kernel
 
 def get_mean_kernel(height,width):
@@ -95,7 +135,14 @@ def get_mean_kernel(height,width):
     mean_kernel = mean_kernel / (height * width)
     return mean_kernel
 
-def get_laplacian_kernel():
+def get_laplacian_kernel(size, center_coefficient):
+    if center_coefficient == 1:
+        coefficient=-1
+    else:
+        coefficient=1
+    laplacian_kernel=np.ones((size,size),dtype=int)*coefficient
+    #now updating the center
+    
     
     return laplacian_kernel
 
@@ -103,23 +150,12 @@ def get_log_kernel():
     # Implement LoG kernel generation here
     return log_kernel
 
-def get_sobel_kernel():
-    # Implement Sobel kernel generation here
-    return sobel_kernel
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def get_sobel_kernel(h_v):
+    kernel_h = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    kernel_v = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    return kernel_h if h_v else kernel_v    
+    
+    
 if __name__ == "__main__":
     main()
 
